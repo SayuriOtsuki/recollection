5a6bcefdc1f5b6cbcb952cfd67932d671bc58c80
Sayuri <colorpencil0312@gmail.com>
2014-11-14 16:43:08 +0900

    タッチすると大きさが変わるよう変更

---
+++ Assets/Script/PicutreSelect/PictureSelect.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PictureSelect : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.touchCount == 1 && Input.GetTouch (0).phase == TouchPhase.Ended) {
+			Debug.Log("Touched");
+
+			Touch touch = Input.GetTouch(0);
+
+			Vector3 newVector = Camera.main.ScreenToWorldPoint( new Vector3( touch.position.x, touch.position.y, Input.mousePosition.z));
+			
+			Vector2 tapPoint = new Vector2(newVector.x, newVector.y);
+			Collider2D colition2d = Physics2D.OverlapPoint(tapPoint);
+			
+			if(colition2d){
+				Debug.Log ("This is = "+ colition2d.name);
+				colition2d.transform.localScale = new Vector3(1,1,1);
+
+			}
+				
+				
+
+		}
+	
+	}
+}
