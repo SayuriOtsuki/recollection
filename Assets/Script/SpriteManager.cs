065b139beea5b5e5cb61c96fb5d0e69554bd5732
Sayuri <colorpencil0312@gmail.com>
2014-11-16 23:00:51 +0900

    左右に動かすとピタッと止まるようになる

--- Assets/Script/SpriteManager.cs
+++ Assets/Script/SpriteManager.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class SpriteManager : MonoBehaviour {
-	public float moveSpeed = 0.08f;
+	public float moveSpeed = 0.02f;
 	public GameObject spriteLeft;
 	public GameObject spriteMiddle;
 	public GameObject spriteRight;
@@ -14,6 +14,8 @@ public class SpriteManager : MonoBehaviour {
 	Vector3 LeftFirstPos;
 	Vector3 MiddleFirstPos;
 	Vector3 RightFirstPos;
+
+	int moveLimitPos = 7;
 	
 
 	void Start () {
@@ -27,7 +29,7 @@ public class SpriteManager : MonoBehaviour {
 	
 	void Update () {
 		
-		if(Input.touchCount > 1 && Input.GetTouch (0).phase == TouchPhase.Moved){
+		if(Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved){
 			
 			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
 			spriteLeft.transform.Translate(touchDeltaPosition.x * moveSpeed, 0, 0);
@@ -41,33 +43,26 @@ public class SpriteManager : MonoBehaviour {
 		}
 		
 		//ぴたっと止める
-		if(Input.touchCount  > 1 && Input.GetTouch (0).phase == TouchPhase.Ended){
-			
-			//1
-			if(comaFirst.transform.position.y < 5.0f){
+		if(Input.touchCount  > 0 && Input.GetTouch (0).phase == TouchPhase.Ended){
+			Debug.Log ("Ended");
+			//左に動かしたら
+			if(spriteMiddle.transform.position.x < -moveLimitPos){
 
-				comaPos = comaDif*0 + comaFirstY;
-				comaFirst.transform.position = new Vector3(comaFirstX, comaPos, comaFirstZ);
-
-			}
-			
-			//2
-			if(comaFirst.transform.position.y >= 5.0f && comaFirst.transform.position.y  < 11.3f){
-			
-				comaPos = comaDif*1 + comaFirstY;
-				comaFirst.transform.position = new Vector3(comaFirstX, comaPos, comaFirstZ);
+				spriteLeft.transform.position = LeftFirstPos;
+				spriteMiddle.transform.position = MiddleFirstPos;
+				spriteRight.transform.position = RightFirstPos;
 
 			}
-			
-			//3
-			if(comaFirst.transform.position.y >= 11.3f && comaFirst.transform.position.y < 19.8f){
-				comaPos = comaDif*2 + comaFirstY;
-				comaFirst.transform.position = new Vector3(comaFirstX, comaPos, comaFirstZ);
+
+			 //右に動かしたら
+			if(spriteMiddle.transform.position.x > moveLimitPos){
 				
-
+				spriteLeft.transform.position = LeftFirstPos;
+				spriteMiddle.transform.position = MiddleFirstPos;
+				spriteRight.transform.position = RightFirstPos;
+				
 			}
-			
-			
+
 		}
 		
 	}
