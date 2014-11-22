2d5104dfc667d0cf38f76f3a24df057fab0d1a14
Sayuri <colorpencil0312@gmail.com>
2014-11-22 13:17:13 +0900

    ビルド成功

---
+++ Assets/Script/Album/PictIndication.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PictIndication : MonoBehaviour {
+
+//	public Canvas targetText;
+//	
+//	// Use this for initialization
+//	void Start () {
+//		
+//		
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		/*if (Input.touchCount > 0) {
+//            if(Input.GetTouch(0).phase == TouchPhase.Moved){
+//                targetGUIText.text = "x= "+ Input.GetTouch(0).position.x+" y= "+ Input.GetTouch(0).position.y;
+//            }
+//        }*/ 
+//		targetText.text = " ";
+//		
+//		if(Input.touchCount > 0){
+//			
+//			
+//			foreach (Touch touch in Input.touches) {
+//				
+//				
+//				Vector3 tapPoint =  Camera.main.ScreenToWorldPoint( new Vector3( touch.position.x, touch.position.y,  0));
+//				
+//				targetText.text += "Finger"  +touch.fingerId + ": x= "+ tapPoint.x.ToString("f2") +" y= "+ tapPoint.y.ToString("f2") + "\n ";
+//				
+//			}
+//			
+//		}
+//	}
+}
