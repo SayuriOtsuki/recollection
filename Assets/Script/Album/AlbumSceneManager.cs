6d7b6ad3cae35a2b0c7f615638cfa34925a8aafb
Sayuri <colorpencil0312@gmail.com>
2014-11-24 17:34:07 +0900

    アルバムシーンを1からに変更（今までは2からだった）

--- Assets/Script/Album/AlbumSceneManager.cs
+++ Assets/Script/Album/AlbumSceneManager.cs
@@ -16,7 +16,14 @@ public class AlbumSceneManager : MonoBehaviour {
 	void OnGUI(){
 		if (GUI.Button (new Rect (300, 300, 100, 50), "Home")) {
 			Resources.UnloadUnusedAssets();
+			Debug.Log("Home");
 			Application.LoadLevel ("StartScene");
 		}
+		if (GUI.Button (new Rect (400, 300, 100, 50), "Tabaco")) {
+			Resources.UnloadUnusedAssets();
+			Debug.Log("tabaco");
+			Application.LoadLevel ("tabaco");
+		}
+
 	}
 }
