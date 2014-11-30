1c220b755e0c773c1a9a880c0c15356146966aa8
Sayuri <colorpencil0312@gmail.com>
2014-12-01 00:23:54 +0900

    プレハブで操作できるよう作成開始。呼び出したプレハブは削除できない…（Destroying assets is not permitted to avoid data loss. If you really want to remove an asset use DestroyImmediate (theObject, true);）

---
+++ Assets/Script/Album/PrefabManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefabManager : MonoBehaviour {
+	public GameObject[] imageBoards;
+	int currentPictureNum = -1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnGUI(){
+		int LButton = 100;
+		int RButton = 800;
+		int height = 300;
+		int width = 100;
+		int tateSize = 50;
+
+		if (GUI.Button (new Rect (LButton, height, width, tateSize), "L")) {
+
+			Debug.Log("L");
+
+			currentPictureNum ++;
+			int nextPictureNum = currentPictureNum + 1;
+			GameObject.Destroy(imageBoards[currentPictureNum]);
+			Instantiate(imageBoards[nextPictureNum],new Vector3(0,0,0),Quaternion.identity);
+
+		}
+		if (GUI.Button (new Rect (RButton, height, width, tateSize), "R")) {
+
+			Debug.Log("R");
+
+		}
+	}
+}
---
+++ Assets/Script/Album/SpriteManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Collections.Generic;
+
+public struct ImageBoard {
+	private int id;
+	private string name;
+
+	public void setName(string name) {
+		this.name = name;
+	}
+
+	public ImageBoard(int id, string name) {
+		this.id = id;
+		this.name = name;
+
+		//this.aaa = new AAA ();
+	}
+
+	/*public ~ImageBoard() {
+		delete this.aaa;
+	}*/
+
+}
+
+public class SpriteManager : MonoBehaviour {
+	public GameObject[] imageBoards;
+
+	private const int NUM_LIST = 3;
+	//public ImageBoard[] imageBoards = new ImageBoard[NUM_LIST];
+
+	private int currentID = 0;
+
+	// Use this for initialization
+	void Start () {
+		List<ImageBoard> _imageBoards = new List<ImageBoard>();
+
+		for (int i = 0; i < NUM_LIST; i++) {
+			ImageBoard ib = new ImageBoard( 0, "Otsuki" );
+			//this.imageBoards.Add(ib);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//this.imageBoards.RemoveAt (0);
+	}
+}
