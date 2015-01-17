54fc7bb0890e99d9d2ae8dd2f30677fce66251a3
Sayuri <colorpencil0312@gmail.com>
2015-01-17 18:52:59 +0900

    ボタン作り直し

--- Assets/Script/Album/PrefabManager.cs
+++ Assets/Script/Album/PrefabManager.cs
@@ -28,7 +28,7 @@ public class PrefabManager : MonoBehaviour {
 
 	public void OnMoveToRight(){
 		//前のプレハブ削除
-		GameObject currentPrefab = GameObject.Find("sample_" + currentNum.ToString() + "(Clone)");
+		GameObject currentPrefab = GameObject.Find("recol_" + currentNum.ToString() + "(Clone)");
 		Destroy(currentPrefab);
 
 		//プレハブ作成
@@ -38,7 +38,7 @@ public class PrefabManager : MonoBehaviour {
 		else{
 			currentNum ++; 
 		}
-		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/sample_" + currentNum.ToString());
+		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/recol_" + currentNum.ToString());
 		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
 		Debug.Log ("currentNum = "+ currentNum);
 
@@ -57,7 +57,7 @@ public class PrefabManager : MonoBehaviour {
 
 	public void OnMoveToLeft(){
 		//前のプレハブ削除
-		GameObject currentPrefab = GameObject.Find("sample_" + currentNum.ToString() + "(Clone)");
+		GameObject currentPrefab = GameObject.Find("recol_" + currentNum.ToString() + "(Clone)");
 		Destroy(currentPrefab);
 		Debug.Log ("DestroyLeft");
 		//プレハブ作成
@@ -70,7 +70,7 @@ public class PrefabManager : MonoBehaviour {
 			currentNum --;
 
 		}
-		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/sample_" + currentNum.ToString());
+		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/recol_" + currentNum.ToString());
 		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
 		Debug.Log ("currentNum = "+ currentNum);
 
@@ -93,14 +93,14 @@ public class PrefabManager : MonoBehaviour {
 	}
 
 	public void All(){
-		GameObject currentPrefab = GameObject.Find("sample_" + currentNum.ToString() + "(Clone)");
+		GameObject currentPrefab = GameObject.Find("recol_" + currentNum.ToString() + "(Clone)");
 		Destroy(currentPrefab);
 		
 		currentNum = 1;
 		int allMinNum = 1;
 		int allMaxNum = 189;		
 		
-		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/sample_" + allMinNum.ToString());
+		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/recol_" + allMinNum.ToString());
 		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
 		Debug.Log ("currentNum = "+ currentNum);
 		
@@ -115,55 +115,126 @@ public class PrefabManager : MonoBehaviour {
 	}
 
 	
-	public void SanrinAuto(){
-		GameObject currentPrefab = GameObject.Find("sample_" + currentNum.ToString() + "(Clone)");
+	public void Home(){
+		GameObject currentPrefab = GameObject.Find("recol_" + currentNum.ToString() + "(Clone)");
 		Destroy(currentPrefab);
 			
-		currentNum = 1;
-		int autoMinNum = 1;
-		int autoMaxNum = 4;
+		int homeMinNum = 1;
+		currentNum = homeMinNum;
+		int homeMaxNum = 15;
 			
-		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/sample_" + autoMinNum.ToString());
+		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/recol_" + homeMinNum.ToString());
 		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
 		Debug.Log ("currentNum = "+ currentNum);
 			
-		maxNum = autoMaxNum;
-		minNum = autoMinNum;
+		maxNum = homeMaxNum;
+		minNum = homeMinNum;
 
 		displayCurrentNum = 1;
-		displayMaxNum = autoMaxNum - autoMinNum + 1;
+		displayMaxNum = homeMaxNum - homeMinNum + 1;
 		numberText.text = displayCurrentNum + " / " + displayMaxNum;
 
 	}
-	public void Camera(){
-		GameObject currentPrefab = GameObject.Find("sample_" + currentNum.ToString() + "(Clone)");
+	public void Curry(){
+		GameObject currentPrefab = GameObject.Find("recol_" + currentNum.ToString() + "(Clone)");
 		Destroy(currentPrefab);
+
+		int curryMinNum = 16;
+		currentNum = curryMinNum;
+		int curryMaxNum = 23;
 		
-		currentNum = 5;
-		int cameraMinNum = 5;
-		int cameraMaxNum = 5;
-		
-		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/sample_" + cameraMinNum.ToString());
+		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/recol_" + curryMinNum.ToString());
 		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
 		Debug.Log ("currentNum = "+ currentNum);
 		
-		maxNum = cameraMaxNum;
-		minNum = cameraMinNum;
+		maxNum = curryMaxNum;
+		minNum = curryMinNum;
 
 		displayCurrentNum = 1;
-		displayMaxNum = cameraMaxNum - cameraMinNum + 1;
+		displayMaxNum = curryMaxNum - curryMinNum + 1;
 		numberText.text = displayCurrentNum + " / " + displayMaxNum;
 
 	}
+
+	public void Kitchen(){
+		GameObject currentPrefab = GameObject.Find("recol_" + currentNum.ToString() + "(Clone)");
+		Destroy(currentPrefab);
+		
+		
+		int kitchenMinNum = 24;
+		currentNum = kitchenMinNum;
+		int kitchenMaxNum = 28;
+		
+		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/recol_" + kitchenMinNum.ToString());
+		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
+		Debug.Log ("currentNum = "+ currentNum);
+		
+		maxNum = kitchenMaxNum;
+		minNum = kitchenMinNum;
+		
+		displayCurrentNum = 1;
+		displayMaxNum = kitchenMaxNum - kitchenMinNum + 1;
+		numberText.text = displayCurrentNum + " / " + displayMaxNum;
+		
+	}
+
+	public void Nougu(){
+		GameObject currentPrefab = GameObject.Find("recol_" + currentNum.ToString() + "(Clone)");
+		Destroy(currentPrefab);
+		
+		
+		int nouguMinNum = 29;
+		currentNum = nouguMinNum;
+		int nouguMaxNum = 32;
+		
+		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/recol_" + nouguMinNum.ToString());
+		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
+		Debug.Log ("currentNum = "+ currentNum);
+		
+		
+		maxNum = nouguMaxNum;
+		minNum = nouguMinNum;
+		
+		displayCurrentNum = 1;
+		displayMaxNum = nouguMaxNum - nouguMinNum + 1;
+		numberText.text = displayCurrentNum + " / " + displayMaxNum;
+		
+		
+	}
+
+	public void TownScene(){
+		GameObject currentPrefab = GameObject.Find("recol_" + currentNum.ToString() + "(Clone)");
+		Destroy(currentPrefab);
+		
+		
+		int tsMinNum = 33;
+		currentNum = tsMinNum;
+		int tsMaxNum = 44;
+		
+		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/recol_" + tsMinNum.ToString());
+		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
+		Debug.Log ("currentNum = "+ currentNum);
+		
+		maxNum = tsMaxNum;
+		minNum = tsMinNum;
+		
+		displayCurrentNum = 1;
+		displayMaxNum = tsMaxNum - tsMinNum + 1;
+		numberText.text = displayCurrentNum + " / " + displayMaxNum;
+		
+		
+	}
+
 	public void Dagashi(){
-		GameObject currentPrefab = GameObject.Find("sample_" + currentNum.ToString() + "(Clone)");
+		GameObject currentPrefab = GameObject.Find("recol_" + currentNum.ToString() + "(Clone)");
 		Destroy(currentPrefab);
 		
-		currentNum = 6;
-		int dagashiMinNum = 6;
-		int dagashiMaxNum = 23;
+
+		int dagashiMinNum = 45;
+		currentNum = dagashiMinNum;
+		int dagashiMaxNum = 61;
 		
-		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/sample_" + dagashiMinNum.ToString());
+		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/recol_" + dagashiMinNum.ToString());
 		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
 		Debug.Log ("currentNum = "+ currentNum);
 		
@@ -177,205 +248,18 @@ public class PrefabManager : MonoBehaviour {
 
 	}
 
-	public void DrinkFood(){
-		GameObject currentPrefab = GameObject.Find("sample_" + currentNum.ToString() + "(Clone)");
-		Destroy(currentPrefab);
-		
-		currentNum = 24;
-		int fdMinNum = 24;
-		int fdMaxNum = 52;
-		
-		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/sample_" + fdMinNum.ToString());
-		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
-		Debug.Log ("currentNum = "+ currentNum);
-		
-		
-		maxNum = fdMaxNum;
-		minNum = fdMinNum;
 
-		displayCurrentNum = 1;
-		displayMaxNum = fdMaxNum - fdMinNum + 1;
-		numberText.text = displayCurrentNum + " / " + displayMaxNum;
-
-
-	}
-
-	public void HomeScene(){
-		GameObject currentPrefab = GameObject.Find("sample_" + currentNum.ToString() + "(Clone)");
-		Destroy(currentPrefab);
-		
-		currentNum = 53;
-		int houseMinNum = 53;
-		int houseMaxNum = 75;
-		
-		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/sample_" + houseMinNum.ToString());
-		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
-		Debug.Log ("currentNum = "+ currentNum);
-		
-		maxNum = houseMaxNum;
-		minNum = houseMinNum;
-
-		displayCurrentNum = 1;
-		displayMaxNum = houseMaxNum - houseMinNum + 1;
-		numberText.text = displayCurrentNum + " / " + displayMaxNum;
-
-
-	}
-
-	public void SignBoard(){
-		GameObject currentPrefab = GameObject.Find("sample_" + currentNum.ToString() + "(Clone)");
-		Destroy(currentPrefab);
-		
-		currentNum = 76;
-		int sbMinNum = 76;
-		int sbMaxNum = 93;
-		
-		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/sample_" + sbMinNum.ToString());
-		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
-		Debug.Log ("currentNum = "+ currentNum);
-		
-		maxNum = sbMaxNum;
-		minNum = sbMinNum;
-
-		displayCurrentNum = 1;
-		displayMaxNum = sbMaxNum - sbMinNum + 1;
-		numberText.text = displayCurrentNum + " / " + displayMaxNum;
-	}
-
-	public void Kitchen(){
-		GameObject currentPrefab = GameObject.Find("sample_" + currentNum.ToString() + "(Clone)");
-		Destroy(currentPrefab);
-		
-		currentNum = 94;
-		int kitchenMinNum = 94;
-		int kitchenMaxNum = 101;
-		
-		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/sample_" + kitchenMinNum.ToString());
-		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
-		Debug.Log ("currentNum = "+ currentNum);
-		
-		maxNum = kitchenMaxNum;
-		minNum = kitchenMinNum;
-
-		displayCurrentNum = 1;
-		displayMaxNum = kitchenMaxNum - kitchenMinNum + 1;
-		numberText.text = displayCurrentNum + " / " + displayMaxNum;
-
-	}
-
-	public void Others(){
-		GameObject currentPrefab = GameObject.Find("sample_" + currentNum.ToString() + "(Clone)");
-		Destroy(currentPrefab);
-		
-		currentNum = 102;
-		int oiMinNum = 102;
-		int oiMaxNum = 161;
-		
-		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/sample_" + oiMinNum.ToString());
-		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
-		Debug.Log ("currentNum = "+ currentNum);
-		
-		maxNum = oiMaxNum;
-		minNum = oiMinNum;
-
-		displayCurrentNum = 1;
-		displayMaxNum = oiMaxNum - oiMinNum + 1;
-		numberText.text = displayCurrentNum + " / " + displayMaxNum;
-
-	}
-
-	public void Post(){
-		GameObject currentPrefab = GameObject.Find("sample_" + currentNum.ToString() + "(Clone)");
-		Destroy(currentPrefab);
-		
-		currentNum = 162;
-		int postMinNum = 162;
-		int postMaxNum = 162;
-		
-		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/sample_" + postMinNum.ToString());
-		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
-		Debug.Log ("currentNum = "+ currentNum);
-		
-		maxNum = postMaxNum;
-		minNum = postMinNum;
-
-		displayCurrentNum = 1;
-		displayMaxNum = postMaxNum - postMinNum + 1;
-		numberText.text = displayCurrentNum + " / " + displayMaxNum;
-
-	}
-
-	public void Radio(){
-		GameObject currentPrefab = GameObject.Find("sample_" + currentNum.ToString() + "(Clone)");
-		Destroy(currentPrefab);
-		
-		currentNum = 163;
-		int radioMinNum = 163;
-		int radioMaxNum = 163;
-		
-		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/sample_" + radioMinNum.ToString());
-		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
-		Debug.Log ("currentNum = "+ currentNum);
-		
-		maxNum = radioMaxNum;
-		minNum = radioMinNum;
-
-		displayCurrentNum = 1;
-		displayMaxNum = radioMaxNum - radioMinNum + 1;
-		numberText.text = displayCurrentNum + " / " + displayMaxNum;
-
-	}
-
-	public void Town(){
-		GameObject currentPrefab = GameObject.Find("sample_" + currentNum.ToString() + "(Clone)");
-		Destroy(currentPrefab);
-		
-		currentNum = 164;
-		int townMinNum = 164;
-		int townMaxNum = 172;
-		
-		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/sample_" + townMinNum.ToString());
-		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
-		Debug.Log ("currentNum = "+ currentNum);
-		
-		maxNum = townMaxNum;
-		minNum = townMinNum;
-
-		displayCurrentNum = 1;
-		displayMaxNum = townMaxNum - townMinNum + 1;
-		numberText.text = displayCurrentNum + " / " + displayMaxNum;
-
-	}
-
-	public void School(){
-		GameObject currentPrefab = GameObject.Find("sample_" + currentNum.ToString() + "(Clone)");
-		Destroy(currentPrefab);
-		
-		currentNum = 173;
-		int schoolMinNum = 173;
-		int schoolMaxNum = 174;
-		
-		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/sample_" + schoolMinNum.ToString());
-		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
-		Debug.Log ("currentNum = "+ currentNum);
-		
-		maxNum = schoolMaxNum;
-		minNum = schoolMinNum;
-
-		displayCurrentNum = 1;
-		displayMaxNum = schoolMaxNum - schoolMinNum + 1;
-		numberText.text = displayCurrentNum + " / " + displayMaxNum;
-	}
 
 	public void Tabaco(){
-		GameObject currentPrefab = GameObject.Find("sample_" + currentNum.ToString() + "(Clone)");
+		GameObject currentPrefab = GameObject.Find("recol_" + currentNum.ToString() + "(Clone)");
 		Destroy(currentPrefab);
 		
-		currentNum = 175;
-		int tabacoMinNum = 175;
-		int tabacoMaxNum = 184;
+
+		int tabacoMinNum = 62;
+		currentNum = tabacoMinNum;
+		int tabacoMaxNum = 70;
 		
-		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/sample_" + tabacoMinNum.ToString());
+		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/recol_" + tabacoMinNum.ToString());
 		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
 		Debug.Log ("currentNum = "+ currentNum);
 		
@@ -386,68 +270,31 @@ public class PrefabManager : MonoBehaviour {
 		displayMaxNum = tabacoMaxNum - tabacoMinNum + 1;
 		numberText.text = displayCurrentNum + " / " + displayMaxNum;
 
-	}
 
-	public void Phone(){
-		GameObject currentPrefab = GameObject.Find("sample_" + currentNum.ToString() + "(Clone)");
+	}
+	
+
+	public void Horo(){
+		GameObject currentPrefab = GameObject.Find("recol_" + currentNum.ToString() + "(Clone)");
 		Destroy(currentPrefab);
 		
-		currentNum = 185;
-		int phoneMinNum = 185;
-		int phoneMaxNum = 187;
+
+		int horoMinNum = 71;
+		currentNum = horoMinNum;
+		int horoMaxNum = 80;
 		
-		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/sample_" + phoneMinNum.ToString());
+		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/recol_" + horoMinNum.ToString());
 		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
 		Debug.Log ("currentNum = "+ currentNum);
 		
-		maxNum = phoneMaxNum;
-		minNum = phoneMinNum;
-
+		maxNum = horoMaxNum;
+		minNum = horoMinNum;
+		
 		displayCurrentNum = 1;
-		displayMaxNum = phoneMaxNum - phoneMinNum + 1;
+		displayMaxNum = horoMaxNum - horoMinNum + 1;
 		numberText.text = displayCurrentNum + " / " + displayMaxNum;
-
+		
 	}
-	public void TV(){
-		GameObject currentPrefab = GameObject.Find("sample_" + currentNum.ToString() + "(Clone)");
-		Destroy(currentPrefab);
-		
-		currentNum = 188;
-		int tvMinNum = 188;
-		int tvMaxNum = 188;
-		
-		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/sample_" + tvMinNum.ToString());
-		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
-		Debug.Log ("currentNum = "+ currentNum);
-		
-		maxNum = tvMaxNum;
-		minNum = tvMinNum;
-
-		displayCurrentNum = 1;
-		displayMaxNum = tvMaxNum - tvMinNum + 1;
-		numberText.text = displayCurrentNum + " / " + displayMaxNum;
-
-	}
-
-	public void Barbar(){
-		GameObject currentPrefab = GameObject.Find("sample_" + currentNum.ToString() + "(Clone)");
-		Destroy(currentPrefab);
-		
-		currentNum = 189;
-		int barbarMinNum = 189;
-		int barbarMaxNum = 189;
-		
-		GameObject nextPrefab = (GameObject)Resources.Load ("Prefab/Album/sample_" + barbarMinNum.ToString());
-		Instantiate(nextPrefab, new Vector3(0,0,0), Quaternion.identity);
-		Debug.Log ("currentNum = "+ currentNum);
-		
-		maxNum = barbarMaxNum;
-		minNum = barbarMinNum;
-
-		displayCurrentNum = 1;
-		displayMaxNum = barbarMaxNum - barbarMinNum + 1;
-		numberText.text = displayCurrentNum + " / " + displayMaxNum;
-
-	}
+	
 
 }
