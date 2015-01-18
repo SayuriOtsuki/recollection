f3dd3c0315d3afc825d2ec5e233b9090dd02abaa
Sayuri <colorpencil0312@gmail.com>
2015-01-18 23:11:13 +0900

    28の井戸から水が流れ出てくるの完成

---
+++ Assets/Script/Album/ParticleManager_water.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleManager_water : MonoBehaviour {
+
+	ParticleSystem water;
+	// Use this for initialization
+	void Start () {
+		water = GameObject.Find ("water_28").GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.touchCount > 0)
+		{
+			Debug.Log ("Touch");
+			
+			Vector3 screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+			Vector3 newVector = Camera.main.ScreenToWorldPoint( new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+			
+			Vector2 tapPoint = new Vector2(newVector.x, newVector.y);
+			Collider2D colition2d = Physics2D.OverlapPoint(tapPoint);
+			
+			
+			if (colition2d != null)
+			{ 
+				Debug.Log(colition2d.name);
+				water.particleSystem.Play();
+				
+				
+			}else{
+				Debug.Log("NUll");
+			}
+		}
+	}
+}
---
+++ Assets/Script/Album/ParticleSortingLayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSortingLayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Set the sorting layer of the particle system.
+		particleSystem.renderer.sortingLayerName = "foreground";
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
