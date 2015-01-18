ce172010c0a5dee9d77634c6f10b1400982e6a18
Sayuri <colorpencil0312@gmail.com>
2015-01-19 01:22:16 +0900

    12の時計が「ボーン」って鳴るのに成功

--- Assets/Script/Album/SoundManager.cs
+++ Assets/Script/Album/SoundManager.cs
@@ -13,23 +13,26 @@ public class SoundManager : MonoBehaviour {
 	void Update () {
 		if (Input.touchCount > 0)
 		{
-			Debug.Log ("Touch");
-			
-			Vector3 screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-			Vector3 newVector = Camera.main.ScreenToWorldPoint( new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-			
-			Vector2 tapPoint = new Vector2(newVector.x, newVector.y);
-			Collider2D colition2d = Physics2D.OverlapPoint(tapPoint);
-			
-			
-			if (colition2d != null)
-			{ 
-				Debug.Log(colition2d.name);
-				audioSource = colition2d.GetComponent<AudioSource>();
-				audioSource.Play();
+			Touch touch = Input.GetTouch(0);
+			if(touch.phase == TouchPhase.Ended){
+				Debug.Log ("Touch");
 				
-			}else{
-				Debug.Log("NUll");
+				Vector3 screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+				Vector3 newVector = Camera.main.ScreenToWorldPoint( new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+				
+				Vector2 tapPoint = new Vector2(newVector.x, newVector.y);
+				Collider2D colition2d = Physics2D.OverlapPoint(tapPoint);
+				
+				
+				if (colition2d != null)
+				{ 
+					Debug.Log(colition2d.name);
+					audioSource = colition2d.GetComponent<AudioSource>();
+					audioSource.Play();
+					
+				}else{
+					Debug.Log("NUll");
+				}
 			}
 		}
 	}
